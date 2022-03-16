c7c36b240143082493da3df028fdfa11a8c65f0a
park71405 <80095068+park71405@users.noreply.github.com>
2022-03-16 23:22:42 +0900

    합병정렬 기초

---
+++ 62/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+int data[10], tmp[10];
+
+void divide(int lt, int rt){
+	int mid;
+	int p1, p2, p3;
+	if(lt < rt){
+		mid = (lt+rt)/2;
+		divide(lt, mid);
+		divide(mid+1, rt);
+		p1 = lt;	//합치기 시작 
+		p2 = mid+1;
+		p3 = lt;
+		while(p1<=mid && p2<=rt){
+			if(data[p1] < data[p2]) tmp[p3++] = data[p1++];
+			else tmp[p3++] = data[p2++];
+		}
+		while(p1<=mid) tmp[p3++] = data[p1++];
+		while(p2<=rt) tmp[p3++] = data[p2++];
+		for(int i=lt; i<=rt; i++){
+			data[i] = tmp[i];
+		}
+	}
+}
+
+int main(int argc, char** argv) {
+	
+	int n, i;
+	scanf("%d", &n);
+	
+	for(i=1; i<=n; i++)
+		scanf("%d", &data[i]);
+	
+	divide(1, n);
+	for(i=1; i<=n; i++)
+		printf("%d ", data[i]);
+	
+	return 0;
+}
