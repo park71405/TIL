36740c032e20bb76775d6459635e5aa2269c058d
park71405 <80095068+park71405@users.noreply.github.com>
2022-03-18 14:45:59 +0900

    미로탐색(DFS)

---
+++ 65/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+int map[8][8], ch[8][8];
+int dx[4] = {-1, 0, 1, 0};
+int dy[4] = {0, 1, 0, -1};
+int cnt=0;
+
+void DFS(int x, int y){
+	int i, xx, yy;
+	if(x == 7 && y == 7){
+		cnt++;
+	}
+	else{
+		for(i=0; i<4; i++){
+			xx = x + dx[i];
+			yy = y + dy[i];
+			if(xx<1 || xx>7 || yy<1 || yy>7) continue;
+			if(map[xx][yy] == 0 && ch[xx][yy] == 0){
+				ch[xx][yy] = 1;
+				DFS(xx, yy);
+				ch[xx][yy] = 0;
+			}
+		}
+	}
+}
+
+int main() {
+	
+	int i, j;
+	for(i=1; i<=7; i++){
+		for(j=1; j<=7; j++){
+			scanf("%d", &map[i][j]);
+		}
+	}
+	ch[1][1] = 1;
+	DFS(1, 1);
+	
+	printf("%d", cnt);
+
+	return 0;
+}
