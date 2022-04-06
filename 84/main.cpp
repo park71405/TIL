149601e726d2c77d4cd341b5586da5d89f12abd2
park71405 <80095068+park71405@users.noreply.github.com>
2022-04-06 13:55:55 +0900

    DFS활용

---
+++ 84/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, res=-2147000000;
+vector<int> T, P;
+void DFS(int L, int sum){
+	if(L == n+1){
+		if(sum>res)
+			res = sum;
+	}
+	else{
+		if(L+T[L] <= n+1)
+			DFS(L+T[L], sum+P[L]);
+		DFS(L+1, sum);
+	}
+}
+
+int main() {
+
+	freopen("input.txt", "rt", stdin);
+	int a, b;
+	cin >> n;
+	T.push_back(0);
+	P.push_back(0);
+	for(int i=0; i<n; i++){
+		cin >> a >> b;
+		T.push_back(a);
+		P.push_back(b);
+	}
+	DFS(1, 0);
+	cout << res;
+
+	return 0;
+}
